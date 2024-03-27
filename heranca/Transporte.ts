bb4813bf45627dce2a128463d4de4bea928dc8fc
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-03-27 13:29:26 -0300

    Exemplo Herança

---
+++ heranca/Transporte.ts
@@ -0,0 +1,24 @@
+﻿export class Transporte{
+
+    private _capacidade: number;
+
+	constructor(_capacidade: number) {
+		this._capacidade = _capacidade;
+	}
+
+	public get capacidade(): number {
+		return this._capacidade;
+	}
+
+	public set capacidade(value: number) {
+		this._capacidade = value;
+	}
+   
+   public visualizar(): void{
+        console.log("***********************************************")
+        console.log("Meio de Transporte")
+        console.log("***********************************************")
+        console.log("Capacidade: " + this._capacidade)
+   }
+   
+}
