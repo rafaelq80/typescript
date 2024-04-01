a61224ea88b8b28917102496b7214fe1b7bb4490
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-04-01 12:31:05 -0300

    Arrow Functions

---
+++ arrow/Estados.ts
@@ -0,0 +1,33 @@
+﻿let estados: Array<string> = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Minas Gerais",
+    "Espirito Santo",
+    "Rio Grande do Sul",
+    "Rio Grande do Norte",
+    "Santa Catarina",
+    "Paraná"
+]
+
+let estadosRepetidos: Array<string> = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Minas Gerais",
+    "Espirito Santo",
+    "Rio Grande do Sul",
+    "Rio Grande do Norte",
+    "Santa Catarina",
+    "Paraná",
+    "Rio de Janeiro",
+    "Minas Gerais"
+]
+
+let estadosContemRio = estados.filter(e => e.includes("Rio"));
+
+console.dir(estadosContemRio.sort())
+
+let estadosRioParana = estados.some(e => {
+    return e === "Rio de Janeiro" || e === "Paraná"
+});
+
+console.log(estadosRioParana)
