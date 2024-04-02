2c76a399adad47b5ab202b60d67d53cf7dec8b02
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-04-02 17:06:25 -0300

    Update

---
+++ exceptions/Conversor.ts
@@ -0,0 +1,19 @@
+﻿
+try {
+
+    const resultado = converte(123);
+
+    console.log(resultado);
+
+} catch (error) {
+    if (error instanceof TypeError)
+        console.log("TypeError: Tipo de dado incorreto!")
+    else
+        console.log("Erro: " + error)
+} finally{
+    console.log("Bye Bye!")
+}
+
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase();
+}
---
+++ exceptions/ValidaIdade.ts
@@ -0,0 +1,31 @@
+﻿import readline = require("readline-sync");
+import { ExcecaoSimples } from "./ExcecaoSimples";
+
+let loop: boolean = true
+
+do {
+
+    try {
+
+        let idade: number = readline.questionInt("Digite a idade: ")
+        validarIdade(idade)
+
+        loop = false
+
+    } catch (error) {
+        if( error instanceof ExcecaoSimples){
+            console.error(error.name)
+            console.error(error.message)
+            console.error("Erro: Digite uma idade maior do que 18 anos!")
+        }else
+            console.error(error)
+    }
+
+} while (loop)
+
+function validarIdade(idade: number): void {
+    if (idade < 18)
+        throw new ExcecaoSimples("A pessoa é menor de idade!")
+    else
+        console.log("A pessoa é maior de idade!")
+}
