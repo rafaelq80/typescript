82f46207e3611f380df1887f00444083bc34a44f
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-06-30 23:05:24 -0300

    Update

---
+++ assincrono/Promises.ts
@@ -0,0 +1,28 @@
+﻿function somar(numero1: number, numero2: number) {
+	return new Promise((resolve, reject) => {
+		let result = numero1 + numero2;
+		if (result > 0) {
+			resolve(result);
+		} else {
+			reject('O resultado é inválido');
+		}
+	});
+}
+
+//console.log(somar(2, 5));
+
+// somar(2, 5)
+// 	.then(resultado => {
+// 		console.log(`O resultado é ${resultado}`);
+// 	})
+// 	.catch(error => {
+// 		console.error(`Ocorreu um erro: ${error}`);
+// 	});
+
+// async function soma(){
+// 	await somar(2,5).then(resultado => {;
+//         console.log(resultado);
+//     });
+// }
+
+// soma()
--- colecoes/ColecaoArray.ts
+++ colecoes/ColecaoArray.ts
@@ -1,4 +1,5 @@
-﻿
+﻿import readline = require('readline-sync');
+
 const numeros: Array<number> = new Array<number>();
 
 // Adicionar um elemento de cada vez
@@ -10,7 +11,11 @@ numeros.push(1)
 // Adiciona vários elementos de uma vez só
 numeros.unshift(2.5, 4.5)
 
-console.table(numeros)
+// Adiciona elemento recebido via teclado
+numeros.push(readline.questionFloat("Digite um numero: "))
+
+for (let numero of numeros)
+    console.log(numero)
 
 console.log("A posição do elemento 5 é: " + numeros.indexOf(5))
 
@@ -21,11 +26,15 @@ console.log("O elemento 10 existe? " + numeros.includes(10))
 // Atualizar os dados de um elemento
 numeros[numeros.indexOf(7)] = 10;
 
+console.log('\nLista Numeros - Com o elemento atualizado\n')
+
 console.table(numeros)
 
 // Apagar um elemento do Array
 // Primeiro parâmetro é indice do elemento
 // Segundo parâmetros é quantos elementos vc deseja apagar a partir do indice
 numeros.splice(numeros.indexOf(10), 1)
+
+console.log('\nLista Numeros - Sem o elemento excluído\n')
 
 console.table(numeros)
